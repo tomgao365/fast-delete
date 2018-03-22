2d6514ff814e14f62f521bd3daa0fc169dd22c5a
Tom Gao <tom@tomgao.cc>
2018-03-22 15:31:09 +0800

    增加删除时间提示

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ if (args.length === 0) {
 
 try {
   console.log('被删除文件夹路径如下：');
+  const startData = new Date();
   args.forEach((file) => {
     console.log(`  ${file}`);
     fs.removeSync(file);
   });
-  console.log('删除工作完成');
+  const endData = new Date();
+  console.log(`删除工作完成，耗时${(endData.getTime()-startData.getTime())/1000}秒`);
 } catch (error) {
   console.log(error.message || '删除失败！');
 }
